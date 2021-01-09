184c4f35cdf8dd5714b442208745963981683f61
Chase Wood <chase_wood07@yahoo.com>
2021-01-09 10:29:21 -0600

    math

---
+++ math.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+echo 56+45;
+echo "<br>";
+echo 56 - 45; 
+echo "<br>";
+echo 56/45;
+echo "<br>";
+echo 56 * 45;
+echo "<br>";
+echo 45 + 34 * 45 / 421 - 45;
+echo "<br>";
+
+// ORDER OF OPERATIONS - SAME AS SCHOOL
+echo 5 + 5 * 10;
+echo "<br>";
+echo (5+5) * 10;
+echo "<br>";
+
+$number = 12;
+$number1 = 243;
+
+echo $number * $number1;
+
+?>
+    
+</body>
+</html>
---
+++ saveAs.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<?php 
+
+$name = "<h1>Chase</h1>";
+$NAME = 'Chase';
+$number = 100;
+$numberList = 234;
+$first_Last = 'Chase Wood';
+$First_Last = 'Hannah Wood';
+
+// BAD FORM
+// $first-Last = 'Chase Wood'
+
+echo $name;
+
+// CONCATENATION
+echo $ $name.$number
+
+
+?>
+
+
+    
+</body>
+</html>
