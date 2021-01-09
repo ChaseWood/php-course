5ab563dc34617d7627be3b48ab72d3ec0c57e3f8
Chase Wood <chase_wood07@yahoo.com>
2021-01-09 15:30:39 -0600

    if statments, switch statments, comparision operators

---
+++ switchStatement.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+$number = 34;
+
+switch($number){
+
+    case 36: 
+        echo 'is it 34';
+        break;
+    case 37: 
+        echo 'is it 34';
+        break;
+    case 35: 
+        echo 'is it 34';
+        break;
+    case 40: 
+        echo 'is it 34';
+        break;
+    case 34: 
+        echo 'it is 34';
+        break;
+
+    default: 
+    echo 'nothing found';
+    
+}
+
+?>
+
+
+
+    
+</body>
+</html>
