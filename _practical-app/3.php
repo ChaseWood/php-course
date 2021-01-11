6b163d3a816af3ae68d2e53f1f98c896895da9ff
Chase Wood <chase_wood07@yahoo.com>
2021-01-11 09:44:47 -0600

    practice 3

--- _practical-app/3.php
+++ _practical-app/3.php
@@ -14,16 +14,47 @@
 
 <?php  
 
-/*  Step1: Make an if Statement with elseif and else to finally display string saying, I love PHP
+// Step1: Make an if Statement with elseif and else to finally display string saying, I love PHP
+$number = 0;
+if($number > 5){
+	echo 'PHP is great';
+}else if($number>10){
+	echo ' PHP is awesome';
+}else{
+	echo 'I love PHP';
+};
+
+echo '<br>';
+
+// 	Step 2: Make a forloop  that displays 10 numbers
+
+for($i = 1; $i<=10; $i++){
+	echo $i . '<br>';
+}
+
+echo '<br>';
 
 
-
-	Step 2: Make a forloop  that displays 10 numbers
-
-
-	Step 3 : Make a switch Statement that test againts one condition with 5 cases
-
- */
+// 	Step 3 : Make a switch Statement that test against one condition with 5 cases
+$number = 34;
+switch($number){
+		case 30:
+		echo 'this number is 30';
+		break;
+		case 31:
+		echo 'this number is 31';
+		break;
+		case 32:
+		echo 'this number is 32';
+		break;
+		case 33:
+		echo 'this number is 33';
+		break;
+		case 34:
+		echo 'this number is 34';
+		break;
+	}
+ 
 
 	
 ?>
