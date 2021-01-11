9163de3868eb8cbab8c05e87c8040a7f31b15ab7
Chase Wood <chase_wood07@yahoo.com>
2021-01-11 11:51:52 -0600

    practice 5

--- _practical-app/5.php
+++ _practical-app/5.php
@@ -15,15 +15,33 @@
 	<?php 
 
 
-/*  Step1: Use a pre-built math function here and echo it
+// Step1: Use a pre-built math function here and echo it
+
+$num = 45.3;
+echo ceil($num);
+echo '<br>';
+echo floor($num);
+echo '<br>';
 
 
-	Step 2:  Use a pre-built string function here and echo it
+// 	Step 2:  Use a pre-built string function here and echo it
 
+$str = 'This IS MY string';
+echo strtoupper($str);
+echo '<br>';
+echo strtolower($str);
+echo '<br>';
+echo strlen($str);
+echo '<br>';
 
-	Step 3:  Use a pre-built Array function here and echo it
+// 	Step 3:  Use a pre-built Array function here and echo it
 
- */
+ $arr = [1,2,3,45,53424,23,3,4,5,23,55,112];
+ echo max($arr);
+ echo '<br>';
+ echo min($arr);
+ echo '<br>';
+ echo sort($arr);
 
 	
 ?>
