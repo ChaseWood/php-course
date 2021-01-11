7500915697b798e450bc2e5a1b330884c2a7a1a6
Chase Wood <chase_wood07@yahoo.com>
2021-01-11 10:06:22 -0600

    function parameters

---
+++ functionParams.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+// function greeting($string){
+
+//     echo $string;
+
+// };
+
+// greeting('this is message in the string param');
+
+
+function calculate($num1, $num2){
+
+    $sum = $num1 + $num2;
+    echo $sum;
+
+};
+
+calculate(12,23);
+
+
+
+?>
+
+
+
+    
+</body>
+</html>
