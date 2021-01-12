0db9fcd22d9b83108aaf0f44b905758da12eab29
Chase Wood <chase_wood07@yahoo.com>
2021-01-12 11:05:51 -0600

    form if statment validations

--- form.php
+++ form.php
@@ -2,13 +2,26 @@
 
 if(isSet($_POST["submit"])){
 
+$names = ['Edwin', 'Student', 'Chase', 'Hannah', 'James', 'Tom'];
+$minimum = 5;
+$maximum = 10;
 $username = $_POST["username"];
 $password = $_POST["password"];
 
-echo 'Hello ' . $username;
 
-echo 'Your Password is ' . $password;
-    
+if(strlen($username) < $minimum){
+    echo 'Username has to be longer than five';
+};
+
+if(strlen($username) > $maximum){
+    echo 'Username cannot be longer than 10';
+};
+
+if(!in_array($username, $names)){
+    echo 'Sorry you are not allowed';
+}else{
+    echo 'Welcome';
+}
 
 };
 
